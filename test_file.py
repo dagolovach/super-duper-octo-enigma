b97bd8dc0e288de84e2ce32c6e6501a2162ba582
dgolovach <dmitry.golovach@disneystreaming.com>
2020-12-10 23:04:12 -0700

    adding testing_type function

--- test_file.py
+++ test_file.py
@@ -1,2 +1,10 @@
+
+def testing_type(var) -> bool:
+    return isinstance(var, (int, float))
+
+
 if __name__ == "__main__":
-    printing('Hello World!')
+    if testing_type('str'):
+        print('Int or Float')
+    else:
+        print('Other')
